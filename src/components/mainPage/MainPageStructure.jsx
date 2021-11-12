f174193a46c7b75da8156c17e730a886ded7e6e5
Луиза <luiza@MacBook-Air-Luiza.local>
2021-11-12 17:21:58 +0600

    main page wip"

--- src/components/mainPage/MainPageStructure.jsx
+++ src/components/mainPage/MainPageStructure.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import LeftSideBar from "./LeftSideBar";
+import MainFeed from "./MainFeed";
+import RightSideBar from "./RightSideBar";
+
+
 
 const MainPageStructure = () => {
-  return <div>MAIN</div>;
+  return (
+    <div className="main-page-structure">
+      <LeftSideBar />
+      <MainFeed />
+      <RightSideBar />
+    </div>
+  )
 };
 
 export default MainPageStructure;
---
+++ src/components/mainPage/rightSideBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./mainPage.css"
+
+const RightSideBar = () => {
+    return (
+        <div>
+            <input type="text" />
+        </div>
+    );
+};
+
+export default RightSideBar;
