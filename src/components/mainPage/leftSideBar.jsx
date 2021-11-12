4b398c22bcd4b9837848a7757bc696199454bca4
Луиза <luiza@MacBook-Air-Luiza.local>
2021-11-12 20:01:01 +0600

    implemented logOut

--- src/components/mainPage/leftSideBar.jsx
+++ src/components/mainPage/leftSideBar.jsx
@@ -1,14 +1,64 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import "./mainPage.css"
-
+import HomeIcon from '@mui/icons-material/Home';
+import Grid3x3Icon from '@mui/icons-material/Grid3x3';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import { authContext } from '../../contexts/AuthContext'
+import { Button } from '@mui/material'
+import { Link, useNavigate } from 'react-router-dom';
 const LeftSideBar = () => {
+    const { logOut } = useContext(authContext)
+    let navigate = useNavigate()
+    function handleLogOut() {
+        logOut();
+        localStorage.clear();
+        navigate("/auth")
+
+    }
 
     return (
-        <div>
-            <TwitterIcon style={{ color: "white" }} />
-        </div>
+        <div className="leftBar">
+            <div className="leftSideBar">
+                <div>
+                    <TwitterIcon style={{ color: "white" }} />
+                </div>
+                <div className="left-text">
+                    <Link to="/home" style={{ textDecoration: "none" }} >
+                        <div className="l-t">
+                            <HomeIcon />
+                            <h5>Home</h5>
+                        </div>
+                    </Link>
+                    <Link to="/explore" style={{ textDecoration: "none" }} >
+                        <div className="l-t">
+                            <Grid3x3Icon />
+                            <h5>Explore</h5>
+                        </div>
+                    </Link>
+                    <Link to="/messages" style={{ textDecoration: "none" }} >
+                        <div className="l-t">
+                            <MailOutlineIcon />
+                            <h5>Messages</h5>
+                        </div>
+                    </Link>
+                    <Link to="profile" style={{ textDecoration: "none" }} >
+                        <div className="l-t">
+                            <PersonOutlineIcon />
+                            <h5>Profile</h5>
+                        </div>
+                    </Link>
+                </div>
 
+                <Button variant="contained">Twitter</Button>
+            </div >
+
+            <Button
+                variant="contained"
+                onClick={() => handleLogOut()}
+            >Log Out</Button>
+        </div>
     );
 }
 
