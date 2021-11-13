4461351cebf8fb531af97a3f23d7372665721d54
Луиза <luiza@MacBook-Air-Luiza.local>
2021-11-13 20:17:46 +0600

    work in progress merchPage addModal

--- src/components/merch/MerchRightSide.jsx
+++ src/components/merch/MerchRightSide.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import MerchCard from './MerchCard';
-import MerchRightSideNavbar from './MerchRightSideNavbar';
-
+import { Button } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import AddMerchModal from '../merch/AddMerchModal'
 const MerchRightSide = () => {
-    return (
-        <div>
-            <div className="navbar">
-                <MerchRightSideNavbar />
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
+    return (
+        <div className="rightSideMerch">
+            <div className="merchNavbar">
+                <h3>Merchendise</h3>
+                <div className="m-n">
+                    <Button
+                        variant="contained"
+                        onClick={handleOpen}
+
+                    >Add</Button>
+                    <ShoppingCartIcon />
+                    <AddMerchModal handleClose={handleClose} open={open} />
+                </div>
             </div>
             <div>
 
