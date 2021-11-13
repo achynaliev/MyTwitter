841a24055aaa131cc99ee91cd1a7ed9c21b162b2
Луиза <luiza@MacBook-Air-Luiza.local>
2021-11-13 17:29:23 +0600

    work in progress merchPage

--- src/Routes.js
+++ src/Routes.js
@@ -2,21 +2,28 @@ import React from "react";
 import AuthContextProvider from "./contexts/AuthContext";
 import UserContextProvider from "./contexts/UserContext";
 import TweetContextProvider from "./contexts/TweetContext";
+import MerchContextProvider from "./contexts/MerchContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import AuthPage from "./pages/AuthPage";
+import MerchPage from "./pages/MerchPage";
+
+
 
 const TweeterRoutes = () => {
   return (
     <AuthContextProvider>
       <UserContextProvider>
         <TweetContextProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route exact path="/auth" element={<AuthPage />} />
-              <Route exact path="/" element={<MainPage />} />
-            </Routes>
-          </BrowserRouter>
+          <MerchContextProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route exact path="/" element={<MainPage />} />
+                <Route exact path="/auth" element={<AuthPage />} />
+                <Route exact path="/products" element={<MerchPage />} />
+              </Routes>
+            </BrowserRouter>
+          </MerchContextProvider>
         </TweetContextProvider>
       </UserContextProvider>
     </AuthContextProvider>
