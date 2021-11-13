ab845b94a5fd01d8d952fed589e9394d778a84bc
Atai Chynaliev <chynaliev.ae@gmail.com>
2021-11-13 17:30:29 +0600

    wip tweet creation

--- src/components/tweets/CreateATweet.jsx
+++ src/components/tweets/CreateATweet.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { tweetContext } from "../../contexts/TweetContext";
 import atai from "../../images/atai.jpg";
 import ImageIcon from "@mui/icons-material/Image";
 import GifIcon from "@mui/icons-material/Gif";
@@ -7,14 +8,57 @@ import { Button } from "@mui/material";
 import "./tweets.css";
 
 const CreateATweet = () => {
+  const { createATweet } = useContext(tweetContext);
+  const [tweetInfo, setTweet] = useState({ tweet: "", imageURL: "" });
+
+  function handleTweetUpdate(e) {
+    let userr = { ...tweetInfo, [e.target.name]: e.target.value };
+    setTweet(userr);
+  }
+
+  function handleTweeting() {
+    if (tweetInfo.tweet.length > 0) {
+      let uid = localStorage.getItem("uid");
+      createATweet(tweetInfo.tweet, tweetInfo.imageURL, uid);
+    } else {
+      alert("please add a tweet");
+    }
+  }
+
+  let tweetBtn;
+  if (tweetInfo.tweet === "") {
+    tweetBtn = (
+      <Button
+        onClick={(e) => handleTweeting(e)}
+        variant="contained"
+        size="small"
+      >
+        Tweet
+      </Button>
+    );
+  } else {
+    tweetBtn = (
+      <Button
+        onClick={(e) => handleTweeting(e)}
+        variant="contained"
+        size="small"
+      >
+        Tweet
+      </Button>
+    );
+  }
+
   return (
     <div className="CreateTweetContainer">
       <img src={atai} className="createTweetLogo"></img>
       <div className="createTweetInputContainer">
         <input
           className="createTweetInput"
+          name="tweet"
           type="text"
           placeholder="What's happening?"
+          value={tweetInfo.tweet}
+          onChange={(e) => handleTweetUpdate(e)}
         />
         <div className="lowerInputContainer">
           <div className="lowerInputLogos">
@@ -22,9 +66,7 @@ const CreateATweet = () => {
             <GifIcon color="primary" />
             <EmojiEmotionsIcon color="primary" />
           </div>
-          <Button variant="contained" size="small">
-            Tweet
-          </Button>
+          {tweetBtn}
         </div>
       </div>
     </div>
