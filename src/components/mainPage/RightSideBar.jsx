8803722516118e4e67071ef11f9a1f51203f37b6
Луиза <luiza@MacBook-Air-Luiza.local>
2021-11-14 15:23:52 +0600

    work in progress addModal

--- src/components/mainPage/RightSideBar.jsx
+++ src/components/mainPage/RightSideBar.jsx
@@ -1,11 +1,64 @@
 import { borderRadius } from "@mui/system";
 import React from "react";
 import "./mainPage.css";
+import SearchIcon from '@mui/icons-material/Search';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
 
 const RightSideBar = () => {
   return (
     <div className="input">
-      <input type="search" style={{ borderRadius: "30px" }} />
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          // onChange={(e) => filterPhones("q", e.target.value)}
+          placeholder="Search Twitter"
+          inputProps={{ 'aria-label': 'search' }}
+        />
+      </Search>
     </div>
   );
 };
--- src/components/merch/MerchRightSide.jsx
+++ src/components/merch/MerchRightSide.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import MerchCard from './MerchCard';
-import { Button } from '@mui/material';
+import { Button, Link } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import AddMerchModal from '../merch/AddMerchModal'
+
+
+
 const MerchRightSide = () => {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+    const [age, setAge] = React.useState('');
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
 
     return (
         <div className="rightSideMerch">
             <div className="merchNavbar">
-                <h3>Merchendise</h3>
+                <h3>Merchandise</h3>
                 <div className="m-n">
                     <Button
                         variant="contained"
@@ -19,11 +26,27 @@ const MerchRightSide = () => {
 
                     >Add</Button>
                     <ShoppingCartIcon />
-                    <AddMerchModal handleClose={handleClose} open={open} />
+                    <AddMerchModal handleClose={handleClose} open={open}
+
+                    />
                 </div>
             </div>
-            <div>
 
+            <div className="categories">
+                <h3>Categories</h3>
+                <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                    <h3>T-Shirt</h3>
+                </Link>
+                <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                    <h3>Hat</h3>
+                </Link>
+                <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                    <h3>Scarf</h3>
+                </Link>
+
+            </div>
+
+            <div className="merchCard">
                 <MerchCard />
             </div>
         </div>
