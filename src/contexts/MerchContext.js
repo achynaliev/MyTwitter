03547c1cb348d486aada2bcf699231385cef393e
Atai Chynaliev <chynaliev.ae@gmail.com>
2021-11-15 18:31:33 +0600

    fix a crash

--- src/contexts/MerchContext.js
+++ src/contexts/MerchContext.js
@@ -1,182 +1,182 @@
-import React, { useReducer } from 'react';
-import axios from 'axios';
-import { APImerch } from '../helpers/config'
-import { calcSubPrice, calcTotalPrice } from '../helpers/calcPrice';
+import React, { useReducer } from "react";
+import axios from "axios";
+import { APImerch } from "../helpers/config";
+import { calcSubPrice, calcTotalPrice } from "../helpers/calcPrice";
 
-export const merchContext = React.createContext()
+export const merchContext = React.createContext();
 const INIT_STATE = {
-    merch: [],
-    merchCountInCart: JSON.parse(localStorage.getItem("cart")) ? JSON.parse(localStorage.getItem("cart")).merch.length : 0,
-    cart: null
-}
+  merch: [],
+  merchCountInCart: JSON.parse(localStorage.getItem("cart"))
+    ? JSON.parse(localStorage.getItem("cart")).merch.length
+    : 0,
+  cart: null,
+};
 
 const reducer = (state = INIT_STATE, action) => {
-    switch (action.type) {
-        case "GET_ALL_MERCH":
-            return { ...state, merch: action.payload }
-        case "EDIT_SPECIFIC_MERCH":
-            return { ...state, merch: action.payload }
-        case "ADD_AND_DELETE_MERCH_IN_CART":
-            return { ...state, merchCountInCart: action.payload };
-        case "GET_CART":
-            return { ...state, cart: action.payload };
-        default:
-            return state;
-    }
-}
-
+  switch (action.type) {
+    case "GET_ALL_MERCH":
+      return { ...state, merch: action.payload };
+    case "EDIT_SPECIFIC_MERCH":
+      return { ...state, merch: action.payload };
+    case "ADD_AND_DELETE_MERCH_IN_CART":
+      return { ...state, merchCountInCart: action.payload };
+    case "GET_CART":
+      return { ...state, cart: action.payload };
+    default:
+      return state;
+  }
+};
 
 const MerchContextProvider = (props) => {
-    const [state, dispatch] = useReducer(reducer, INIT_STATE)
+  const [state, dispatch] = useReducer(reducer, INIT_STATE);
 
-    const createMerch = async (merch) => {
-        try {
-            const response = await axios.post(APImerch, merch)
-            let action = {
-                type: "CREATE_MERCH",
-                payload: response.data,
-            }
-            dispatch(action)
-            getAllMerch();
-        } catch (e) {
-            console.log(e)
-        }
-
+  const createMerch = async (merch) => {
+    try {
+      const response = await axios.post(APImerch, merch);
+      let action = {
+        type: "CREATE_MERCH",
+        payload: response.data,
+      };
+      dispatch(action);
+      getAllMerch();
+    } catch (e) {
+      console.log(e);
     }
-    const getAllMerch = async () => {
-        try {
-            const response = await axios(APImerch)
-            console.log("here")
-            let action = {
-                type: "GET_ALL_MERCH",
-                payload: response.data,
-            };
-            dispatch(action)
-
-        } catch (e) {
-            console.log(e)
-        }
+  };
+  const getAllMerch = async () => {
+    try {
+      const response = await axios(APImerch);
+      console.log("here");
+      let action = {
+        type: "GET_ALL_MERCH",
+        payload: response.data,
+      };
+      dispatch(action);
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    const editSpecificMerch = async (id, merch) => {
-        try {
-            const response = await axios(APImerch + id, merch)
-            let action = {
-                type: "EDIT_SPECIFIC_MERCH",
-                payload: response.data,
-            };
-            dispatch(action)
-        } catch (e) {
-            console.log(e)
-        }
+  const editSpecificMerch = async (id, merch) => {
+    try {
+      const response = await axios(APImerch + id, merch);
+      let action = {
+        type: "EDIT_SPECIFIC_MERCH",
+        payload: response.data,
+      };
+      dispatch(action);
+    } catch (e) {
+      console.log(e);
     }
-    const deleteMerch = async (id) => {
-        try {
-            const response = await axios.delete(APImerch + id)
-            let action = {
-                type: "DELETE_MERCH",
-                payload: response.data
-            }
-        } catch (e) {
-            console.log(e)
-        }
+  };
+  const deleteMerch = async (id) => {
+    try {
+      const response = await axios.delete(APImerch + id);
+      let action = {
+        type: "DELETE_MERCH",
+        payload: response.data,
+      };
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    const addAndDeleteMerchInCart = (merch) => {
-        let cart = JSON.parse(localStorage.getItem("cart"))
-        if (!cart) {
-            cart = {
-                merch: [],
-                totalPrice: 0,
-            }
-        }
-        let product = {
-            merch,
-            count: 1,
-            subPrice: 0
-        }
-        product.subPrice = calcSubPrice(product)
-        let checkArr = cart.merch.filter(item => {
-            return item.merch.id === merch.id;
-        });
-        console.log(checkArr)
-        if (checkArr.length === 0) {
-            cart.merch.push(product);
-        } else {
-            cart.merch = cart.merch.filter(item => {
-                return item.merch.id !== merch.id;
-            });
-        }
-
-        cart.totalPrice = calcTotalPrice(cart);
-        localStorage.setItem("cart", JSON.stringify(cart))
-        let action = {
-            type: "ADD_AND_DELETE_MERCH_IN_CART",
-            payload: cart.merch.length,
-
-        };
-        dispatch(action)
+  const addAndDeleteMerchInCart = (merch) => {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    if (!cart) {
+      cart = {
+        merch: [],
+        totalPrice: 0,
+      };
+    }
+    let product = {
+      merch,
+      count: 1,
+      subPrice: 0,
     };
-
-    const checkMerchInCart = (id) => {
-        console.log(id)
-        let cart = JSON.parse(localStorage.getItem("cart"))
-        console.log(cart)
-        let checkArr = cart.merch.filter(item => item.merch.id === id)
-        console.log(checkArr.length === 0)
-        if (checkArr.length === 0) {
-            return false;
-        } else {
-            return true
-        }
-
+    product.subPrice = calcSubPrice(product);
+    let checkArr = cart.merch.filter((item) => {
+      return item.merch.id === merch.id;
+    });
+    console.log(checkArr);
+    if (checkArr.length === 0) {
+      cart.merch.push(product);
+    } else {
+      cart.merch = cart.merch.filter((item) => {
+        return item.merch.id !== merch.id;
+      });
     }
-    const getCart = () => {
-        let cart = JSON.parse(localStorage.getItem("cart"))
-        cart = {
-            merch: [],
-            totalPrice: 0
-        }
-        let action = {
-            type: "GET_CART",
-            payload: cart
-        };
-        dispatch(action)
+
+    cart.totalPrice = calcTotalPrice(cart);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    let action = {
+      type: "ADD_AND_DELETE_MERCH_IN_CART",
+      payload: cart.merch.length,
     };
-    const changeCountMerch = (count, id) => {
-        if (count < 1) {
-            return;
-        }
-        let cart = JSON.parse(localStorage.getItem("cart"))
-        cart.merch = cart.merch.map(item => {
-            if (item.merch.id === id) {
-                item.count = count
-                item.subPrice = calcSubPrice(item)
-            }
-            return item
-        });
-        cart.totalPrice = calcTotalPrice(cart)
-        localStorage.setItem("cart", JSON.stringify(cart))
-        getCart()
+    dispatch(action);
+  };
+
+  const checkMerchInCart = (id) => {
+    console.log(id);
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    if (cart) {
+      let checkArr = cart.merch.filter((item) => item.merch.id === id);
+      console.log(checkArr.length === 0);
+      if (checkArr.length === 0) {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
     }
-    return (
-        <merchContext.Provider
-            value={{
-                createMerch: createMerch,
-                getAllMerch: getAllMerch,
-                editSpecificMerch: editSpecificMerch,
-                deleteMerch: deleteMerch,
-                addAndDeleteMerchInCart: addAndDeleteMerchInCart,
-                checkMerchInCart: checkMerchInCart,
-                getCart: getCart,
-                changeCountMerch: changeCountMerch,
-                merchCountInCart: state.merchCountInCart,
-                merch: state.merch,
-            }}
-        >
-            {props.children}
-        </merchContext.Provider>
-    );
+  };
+  const getCart = () => {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    cart = {
+      merch: [],
+      totalPrice: 0,
+    };
+    let action = {
+      type: "GET_CART",
+      payload: cart,
+    };
+    dispatch(action);
+  };
+  const changeCountMerch = (count, id) => {
+    if (count < 1) {
+      return;
+    }
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    cart.merch = cart.merch.map((item) => {
+      if (item.merch.id === id) {
+        item.count = count;
+        item.subPrice = calcSubPrice(item);
+      }
+      return item;
+    });
+    cart.totalPrice = calcTotalPrice(cart);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    getCart();
+  };
+  return (
+    <merchContext.Provider
+      value={{
+        createMerch: createMerch,
+        getAllMerch: getAllMerch,
+        editSpecificMerch: editSpecificMerch,
+        deleteMerch: deleteMerch,
+        addAndDeleteMerchInCart: addAndDeleteMerchInCart,
+        checkMerchInCart: checkMerchInCart,
+        getCart: getCart,
+        changeCountMerch: changeCountMerch,
+        merchCountInCart: state.merchCountInCart,
+        merch: state.merch,
+      }}
+    >
+      {props.children}
+    </merchContext.Provider>
+  );
 };
 
 export default MerchContextProvider;
