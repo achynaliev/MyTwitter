5f4a26bce5f3d6361756f59b599a8ef2f068327e
Луиза <luiza@MacBook-Air-Luiza.local>
2021-11-18 21:36:26 +0600

    comments context

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -3,3 +3,5 @@ export const APIusers = "http://localhost:8000/users/";
 export const APImerch = "http://localhost:8000/merch/";
 export const APIsmerch = "http://localhost:8000/merch";
 export const APIlikes = "http://localhost:8000/likes/";
+export const APIcomments = "http://localhost:8000/comments/";
+
