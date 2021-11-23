7486453f1f10754f3bd6a2fad94a22c902590432
Atai Chynaliev <chynaliev.ae@gmail.com>
2021-11-23 15:47:34 +0600

    updated config file

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,6 +1,6 @@
-export const APItweets = "https://wu-twitter.herokuapp.com/tweets/";
-export const APIusers = "https://wu-twitter.herokuapp.com/users/";
-export const APImerch = "https://wu-twitter.herokuapp.com/merch/";
-export const APIsmerch = "https://wu-twitter.herokuapp.com/merch";
-export const APIlikes = "https://wu-twitter.herokuapp.com/likes/";
-export const APIcomments = "https://wu-twitter.herokuapp.com/comments/";
+export const APItweets = "https://wu-twitter.herokuapp.com/api/tweets/";
+export const APIusers = "https://wu-twitter.herokuapp.com/api/users/";
+export const APImerch = "https://wu-twitter.herokuapp.com/api/merch/";
+export const APIsmerch = "https://wu-twitter.herokuapp.com/api/merch";
+export const APIlikes = "https://wu-twitter.herokuapp.com/api/likes/";
+export const APIcomments = "https://wu-twitter.herokuapp.com/api/comments/";
