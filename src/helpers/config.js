178629cc0f045bcc586aae904586efa0b0c0c7d7
Atai Chynaliev <chynaliev.ae@gmail.com>
2021-11-23 15:23:20 +0600

    heroku comp

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,7 +1,6 @@
-export const APItweets = "http://localhost:8000/tweets/";
-export const APIusers = "http://localhost:8000/users/";
-export const APImerch = "http://localhost:8000/merch/";
-export const APIsmerch = "http://localhost:8000/merch";
-export const APIlikes = "http://localhost:8000/likes/";
-export const APIcomments = "http://localhost:8000/comments/";
-
+export const APItweets = "https://wu-twitter.herokuapp.com/tweets/";
+export const APIusers = "https://wu-twitter.herokuapp.com/users/";
+export const APImerch = "https://wu-twitter.herokuapp.com/merch/";
+export const APIsmerch = "https://wu-twitter.herokuapp.com/merch";
+export const APIlikes = "https://wu-twitter.herokuapp.com/likes/";
+export const APIcomments = "https://wu-twitter.herokuapp.com/comments/";
